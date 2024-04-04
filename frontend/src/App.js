09f057bb4e2da68b4a531144d49d49d07cbf1d78
ChoiJongMin <cjm8374@kakao.com>
2024-04-04 09:29:07 +0900

    * 카드 ui 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Category-btn-group">
-          <button className="Category-btn">리그 오브 레전드</button>
-          <button className="Category-btn">배틀 그라운드</button>
-          <button className="Category-btn">메이플 스토리</button>
-          <button className="Category-btn">주식/코인</button>
-          <button className="Category-btn">고민상담</button>
-          <button className="Category-btn">기타</button>
+    <div class="app">
+      <header class="app-header">
+        <div class="search">
+          <input class="input" type="text" placeholder="검색어 입력"/>
+          <img class="img" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
+        </div>
+        <div class="category">
+          <button class="btn">리그 오브 레전드</button>
+          <button class="btn">배틀 그라운드</button>
+          <button class="btn">메이플 스토리</button>
+          <button class="btn">주식/코인</button>
+          <button class="btn">고민상담</button>
+          <button class="btn">기타</button>
         </div>
       </header>
-      <main className="App-main">
-        <div className="Search">
-          <input className="Search-input" type="text" placeholder="검색어 입력"/>
-          <img className="Search-img" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
+      <main class="app-main">
+        <div class="chat-list">
+          <div class="card">
+            <div class="title">골드4 듀오 구합니다. 저는 정글러 입니다. 1인분 가능하신분...</div>
+            <div class="count">1/2</div>
+            <div class="user">최박사</div>
+            <div class="date">2024.04.03 17:00:00</div>
+          </div>
         </div>
       </main>
       <footer>
